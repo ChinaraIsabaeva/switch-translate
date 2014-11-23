6c8cd835ef1bfbfcf9ad339f4209bb387ceec603
Chinara <chinara.isabaeva@gmail.com>
2014-11-23 16:01:51 +0000

    change sending data from form data to json

--- static/script.js
+++ static/script.js
@@ -1,12 +1,15 @@
 $(function() {
     $('#button').click(function(){
+      var data = {text: $('#input').val()};
+      console.log(data);
       $.ajax({
 	url: '/',
         type: 'POST',
-	data: $('#input').serialize(),
-        success: function(response) {
-          $('.result').html(response);
-	  $("#input").val('');
+	contentType: 'application/json; charset=utf-8',
+	dataType: 'json',
+	data: JSON.stringify(data),
+        success: function(data) {
+          alert(data);
         }
       });
     });
--- switch_translate.py
+++ switch_translate.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import json
 
 letters = {'q': 'й', 'w': 'ц', 'e' :'у', 'r' :'к', 't' :'е', 'y' :'н', 'u' :'г', 'i' :'ш', 'o' :'щ', 'p' :'з', '[' :'х', ']' :'ъ', 'a' :'ф', 's' :'ы', 'd' :'в', 'f' :'а', 'g' :'п', 'h' :'р', 'j' :'о', 'k' :'л', 'l' :'д', ';' :'ж', "'" :'э', 'z' :'я', 'x' :'ч', 'c' :'с', 'v' :'м', 'b' :'и', 'n' :'т', 'm' :'ь', ' ,' :'б', '.' :'ю', '/': '.', '!': '!', '@': '"', '#': '№', '$': ';', '%': '%', '^': ':', '&': '?', '*': '*', '(': '(', ')': ')', ' ': ' '}
 
@@ -21,7 +22,8 @@ def home():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    result = translate(request.form['text'])
+    data = request.get_json()
+    result = translate(data[text])
     return result
 
 if __name__ == '__main__':
